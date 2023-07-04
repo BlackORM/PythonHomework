dfcb0df7d134b99bf7726e8590504dc45d8dee55
HardDriver <ormo@mail.ru>
2023-07-04 18:48:01 +0300

    Макс кол-во одинаковых элементов списка

--- venv/QA_Python/Homework_Seminar5/task3.py
+++ venv/QA_Python/Homework_Seminar5/task3.py
@@ -1,6 +1,22 @@
+# 3.Создайте список из случайных чисел.
+# Найдите максимальное количество его одинаковых элементов.
+
 import random
 n = int(input('Введите длину списка: '))
 a = []
 max = 0
 for i in range(n):
     a.append(random.randint(1, 10))
+print(a)
+
+count = {}
+
+for num in a:
+    if num in count:
+        count[num] += 1
+    else:
+        count[num] = 1
+    if count[num] > max:
+        max = count[num]
+print(f'Словарь элементов: {count}')
+print (f'Максимальное количество одинаковых элементов: {max}')
